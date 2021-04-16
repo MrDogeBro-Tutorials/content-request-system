fe3ea54a88917a7090e3c3b90cca01fb8cf99c40
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-04-16 18:13:40 -0400

    Add example config file

---
+++ config.example.js
@@ -0,0 +1,15 @@
+// ========= EXAMPLE CONFIG =========
+// NOTE: This is an example config! You will need to plug in values for your
+// own items for some of the config options (if config value is named starting
+// with `example-`, it needs to be replaced)! You also will need to rename this
+// file from `config.example.js` to `config.js` or create a new file that
+// is named config.js and contains the same config values and structure for
+// the script to run and not give errors due to misconfiguration. Check out
+// the GitHub page for more config info. Thank you for reading this note!
+
+// this is the config container ref — it is required for the script to run
+const config = {
+  sheetId: 'example-id', // the id of the sheet to read data from
+  formId: 'example-id', // the id of the form to update/modify
+  numRowsToGet: 500, // the number of rows to initally query for data
+};
