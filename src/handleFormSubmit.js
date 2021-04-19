38b5d111d5276d9735e71b6d854cc117f7b6817a
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-04-19 18:49:42 -0400

    Get data from response sheet

--- src/handleFormSubmit.js
+++ src/handleFormSubmit.js
@@ -1,5 +1,8 @@
 const pushFormData = () => {
   let verifyCols = [];
+  let series = [];
+  let topics = [];
+  let descriptions = [];
 
   let sheet = SpreadsheetApp.openById(config.responseSheetId);
 
@@ -10,4 +13,24 @@ const pushFormData = () => {
   for (let i = 0; i < questionRow[0].length; i++)
     if (questionRow[0][i] == 'Verify question duplicate status.')
       verifyCols.push(i + 1);
+
+  let rawSeries = sheet
+    .getSheetByName(config.responseSheet)
+    .getSheetValues(2, config.responseSeriesColumn, config.numRowsToGet, 1);
+
+  for (let i = 0; i < rawSeries.length; i++)
+    if (rawSeries[i][0].length > 0) series.push(rawSeries[0][i]);
+
+  let rawTopics = sheet
+    .getSheetByName(config.responseSheet)
+    .getSheetValues(2, config.responseTopicColumn, series.length, 1);
+
+  for (let i = 0; i < rawTopics.length; i++) topics.push(rawTopics[i][0]);
+
+  let rawDescriptions = sheet
+    .getSheetByName(config.responseSheet)
+    .getSheetValues(2, config.responseDescriptionColumn, series.length, 1);
+
+  for (let i = 0; i < rawDescriptions.length; i++)
+    descriptions.push(rawTopics[i][0]);
 };
