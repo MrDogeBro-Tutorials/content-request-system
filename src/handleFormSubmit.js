69dbf1a4004e7b3dff79d0e08345b4d552fcbc7e
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-04-19 18:51:04 -0400

    Remove getting verify question cols as it is not needed

--- src/handleFormSubmit.js
+++ src/handleFormSubmit.js
@@ -1,18 +1,9 @@
 const pushFormData = () => {
-  let verifyCols = [];
   let series = [];
   let topics = [];
   let descriptions = [];
 
   let sheet = SpreadsheetApp.openById(config.responseSheetId);
-
-  let questionRow = sheet
-    .getSheetByName(config.responseSheet)
-    .getSheetValues(1, 1, 1, 100);
-
-  for (let i = 0; i < questionRow[0].length; i++)
-    if (questionRow[0][i] == 'Verify question duplicate status.')
-      verifyCols.push(i + 1);
 
   let rawSeries = sheet
     .getSheetByName(config.responseSheet)
